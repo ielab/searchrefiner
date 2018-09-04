e7b8a6930d22e4ab412b1cadd59125ce5f3a5e89
Harry Scells <harryscells@gmail.com>
2018-09-04 16:09:05 +1000

    Integrate plugin functionality.

---
+++ Makefile
@@ -0,0 +1,22 @@
+plugin_dir := plugin
+plugin_files := $(wildcard $(plugin_dir)/*)
+
+go_source = *.go cmd/searchrefiner/*.go
+go_plugin = $(wildcard $(plugin_dir)/*/*.go)
+
+all: searchrefiner plugin
+PHONEY: all
+
+searchrefiner: $(go_source) plugins
+	go build -o server cmd/searchrefiner/server.go
+
+plugins: $(go_plugin)
+	$(foreach dir, $(plugin_files), go build -buildmode=plugin -o $(dir)/plugin.so $(dir)/main.go)
+
+run: all
+	@./server
+
+.PHONEY: clean
+clean:
+	rm server
+	$(foreach dir, $(plugin_files), rm $(dir)/plugin.so)
--- auth.go
+++ auth.go
@@ -1,24 +1,24 @@
-package main
+package searchrefiner
 
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func handleAccountLogin(c *gin.Context) {
+func HandleAccountLogin(c *gin.Context) {
 	c.HTML(http.StatusOK, "account_login.html", nil)
 }
 
-func handleAccountCreate(c *gin.Context) {
+func HandleAccountCreate(c *gin.Context) {
 	c.HTML(http.StatusOK, "account_create.html", nil)
 }
 
-func (s server) apiAccountLogin(c *gin.Context) {
+func (s Server) ApiAccountLogin(c *gin.Context) {
 	var username, password string
 	if v, ok := c.GetPostForm("username"); ok {
 		username = v
 	} else {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "no username supplied", BackLink: "/account/login"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "no username supplied", BackLink: "/account/login"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
@@ -26,7 +26,7 @@ func (s server) apiAccountLogin(c *gin.Context) {
 	if v, ok := c.GetPostForm("password"); ok {
 		password = v
 	} else {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "no password supplied", BackLink: "/account/login"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "no password supplied", BackLink: "/account/login"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
@@ -34,24 +34,24 @@ func (s server) apiAccountLogin(c *gin.Context) {
 	if s.UserState.CorrectPassword(username, password) {
 		err := s.UserState.Login(c.Writer, username)
 		if err != nil {
-			c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: err.Error(), BackLink: "/account/login"})
+			c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: err.Error(), BackLink: "/account/login"})
 			c.AbortWithError(http.StatusUnauthorized, err)
 			return
 		}
 		c.Redirect(http.StatusFound, "/")
 		return
 	}
-	c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "invalid login credentials", BackLink: "/account/login"})
+	c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "invalid login credentials", BackLink: "/account/login"})
 	c.Status(http.StatusUnauthorized)
 	return
 }
 
-func (s server) apiAccountCreate(c *gin.Context) {
+func (s Server) ApiAccountCreate(c *gin.Context) {
 	var username, password, password2 string
 	if v, ok := c.GetPostForm("username"); ok {
 		username = v
 	} else {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "no username supplied", BackLink: "/account/create"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "no username supplied", BackLink: "/account/create"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
@@ -59,7 +59,7 @@ func (s server) apiAccountCreate(c *gin.Context) {
 	if v, ok := c.GetPostForm("password"); ok {
 		password = v
 	} else {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "passwords do not match", BackLink: "/account/create"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "passwords do not match", BackLink: "/account/create"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
@@ -67,19 +67,19 @@ func (s server) apiAccountCreate(c *gin.Context) {
 	if v, ok := c.GetPostForm("password2"); ok {
 		password2 = v
 	} else {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "passwords do not match", BackLink: "/account/create"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "passwords do not match", BackLink: "/account/create"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
 
 	if password != password2 {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "passwords do not match", BackLink: "/account/create"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "passwords do not match", BackLink: "/account/create"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
 
 	if s.UserState.HasUser(username) {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "a user with that name already exists", BackLink: "/account/create"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "a user with that name already exists", BackLink: "/account/create"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
@@ -102,7 +102,7 @@ func (s server) apiAccountCreate(c *gin.Context) {
 
 	err := s.UserState.Login(c.Writer, username)
 	if err != nil {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: err.Error(), BackLink: "/account/create"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: err.Error(), BackLink: "/account/create"})
 		c.AbortWithError(http.StatusUnauthorized, err)
 		return
 	}
@@ -110,7 +110,7 @@ func (s server) apiAccountCreate(c *gin.Context) {
 	return
 }
 
-func (s server) apiAccountLogout(c *gin.Context) {
+func (s Server) ApiAccountLogout(c *gin.Context) {
 	username := s.UserState.Username(c.Request)
 	if s.UserState.IsLoggedIn(username) {
 		s.UserState.Logout(username)
@@ -120,16 +120,16 @@ func (s server) apiAccountLogout(c *gin.Context) {
 	return
 }
 
-func (s server) apiAccountUsername(c *gin.Context) {
+func (s Server) ApiAccountUsername(c *gin.Context) {
 	username := s.UserState.Username(c.Request)
 	c.String(http.StatusOK, username)
 	return
 }
 
-func (s server) handleAdmin(c *gin.Context) {
+func (s Server) HandleAdmin(c *gin.Context) {
 	u, err := s.UserState.AllUnconfirmedUsernames()
 	if err != nil {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: err.Error(), BackLink: "/"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: err.Error(), BackLink: "/"})
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
@@ -141,11 +141,11 @@ func (s server) handleAdmin(c *gin.Context) {
 	c.HTML(http.StatusOK, "admin.html", admin{Unconfirmed: u})
 }
 
-func (s server) apiAdminConfirm(c *gin.Context) {
+func (s Server) ApiAdminConfirm(c *gin.Context) {
 	if v, ok := c.GetPostForm("username"); ok {
 		s.UserState.Confirm(v)
 	} else {
-		c.HTML(http.StatusUnauthorized, "error.html", errorPage{Error: "invalid credentials", BackLink: "/"})
+		c.HTML(http.StatusUnauthorized, "error.html", ErrorPage{Error: "invalid credentials", BackLink: "/"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
