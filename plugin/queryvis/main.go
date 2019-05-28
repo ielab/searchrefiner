bfe7fae9e676f6235d792c0e883f368eac582eef
Harry Scells <harryscells@gmail.com>
2019-05-28 16:49:50 +1000

    Implement new visualisation and fix bugs.

--- plugin/queryvis/main.go
+++ plugin/queryvis/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/hscells/groove/combinator"
 	gpipeline "github.com/hscells/groove/pipeline"
 	"github.com/hscells/transmute"
-	"github.com/hscells/transmute/fields"
 	tpipeline "github.com/hscells/transmute/pipeline"
 	"github.com/ielab/searchrefiner"
 	"net/http"
@@ -29,58 +28,6 @@ func handleTree(s searchrefiner.Server, c *gin.Context) {
 		compiler = v
 	} else {
 		lang = "medline"
-	}
-
-	compiler.Options.FieldMapping = map[string][]string{
-		"Affiliation":                     {fields.Affiliation},
-		"All Fields":                      {fields.AllFields},
-		"Author":                          {fields.Author},
-		"Authors":                         {fields.Authors},
-		"Author - Corporate":              {fields.AuthorCorporate},
-		"Author - First":                  {fields.AuthorFirst},
-		"Author - Full":                   {fields.AuthorFull},
-		"Author - Identifier":             {fields.AuthorIdentifier},
-		"Author - Last":                   {fields.AuthorLast},
-		"Book":                            {fields.Book},
-		"Date - Completion":               {fields.DateCompletion},
-		"Conflict Of Interest Statements": {fields.ConflictOfInterestStatements},
-		"Date - Create":                   {fields.DateCreate},
-		"Date - Entrez":                   {fields.DateEntrez},
-		"Date - MeSH":                     {fields.DateMeSH},
-		"Date - Modification":             {fields.DateModification},
-		"Date - Publication":              {fields.DatePublication},
-		"EC/RN Number":                    {fields.ECRNNumber},
-		"Editor":                          {fields.Editor},
-		"Filter":                          {fields.Filter},
-		"Grant Number":                    {fields.GrantNumber},
-		"ISBN":                            {fields.ISBN},
-		"Investigator":                    {fields.Investigator},
-		"Investigator - Full":             {fields.InvestigatorFull},
-		"Issue":                           {fields.Issue},
-		"Journal":                         {fields.Journal},
-		"Language":                        {fields.Language},
-		"Location ID":                     {fields.LocationID},
-		"MeSH Major Topic":                {fields.MeSHMajorTopic},
-		"MeSH Subheading":                 {fields.MeSHSubheading},
-		"MeSH Terms":                      {fields.MeSHTerms},
-		"Other Term":                      {fields.OtherTerm},
-		"Pagination":                      {fields.Pagination},
-		"Pharmacological Action":          {fields.PharmacologicalAction},
-		"Publication Type":                {fields.PublicationType},
-		"Publisher":                       {fields.Publisher},
-		"Secondary Source ID":             {fields.SecondarySourceID},
-		"Subject Personal Name":           {fields.SubjectPersonalName},
-		"Supplementary Concept":           {fields.SupplementaryConcept},
-		"Floating MeshHeadings":           {fields.FloatingMeshHeadings},
-		"Text Word":                       {fields.TextWord},
-		"Title":                           {fields.Title},
-		"Title/Abstract":                  {fields.TitleAbstract},
-		"Transliterated Title":            {fields.TransliteratedTitle},
-		"Volume":                          {fields.Volume},
-		"MeSH Headings":                   {fields.MeshHeadings},
-		"Major Focus MeSH Heading":        {fields.MajorFocusMeshHeading},
-		"Publication Date":                {fields.PublicationDate},
-		"Publication Status":              {fields.PublicationStatus},
 	}
 
 	fmt.Println(rawQuery)
